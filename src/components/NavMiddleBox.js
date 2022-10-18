ae6043297f2be23936a8aeea481e1898d282a347
zinukk <slsxpseh54@naver.com>
2022-10-18 10:46:52 +0900

    ADD : NavMiddleBox 컴포넌트 추가

--- src/components/NavMiddleBox.js
+++ src/components/NavMiddleBox.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const NavMiddleBox = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="navMiddleBox">
+      <div className="navLogoBox">
+        <p className="navLogoImg">Kurly</p>
+        <p
+          className="navMarketText"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+          마켓컬리
+        </p>
+        <p className="navBeautyText">뷰티컬리</p>
+      </div>
+      <div className="navSearchBox">
+        <input
+          className="navSearchInput"
+          type="text"
+          placeholder="검색어를 입력해주세요"
+        />
+        <FontAwesomeIcon className="navSearchIcon" icon={faHeart} />
+      </div>
+      <div className="navIconBox">
+        <FontAwesomeIcon className="navIcon" icon={faHeart} />
+        <FontAwesomeIcon className="navIcon" icon={faHeart} />
+        <FontAwesomeIcon className="navIcon" icon={faHeart} />
+      </div>
+    </div>
+  );
 };
 
 export default NavMiddleBox;
