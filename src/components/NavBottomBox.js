7a728b626fffe00a5f6c9394bc9ccb6a8a8ef8eb
zinukk <slsxpseh54@naver.com>
2022-10-18 10:48:06 +0900

    ADD : NavBottomBox 컴포넌트 추가

--- src/components/NavBottomBox.js
+++ src/components/NavBottomBox.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const NavBottomBox = () => {
-  return <div></div>;
+  return (
+    <div className="navBottomBox">
+      <div className="navCategoryBox">
+        <FontAwesomeIcon className="navIcon" icon={faHeart} />
+        <p className="navCategoryText">카테고리</p>
+      </div>
+      <div className="navSelectBox">
+        <p className="navSelectText">신상품</p>
+        <p className="navSelectText">베스트</p>
+        <p className="navSelectText">알뜰쇼핑</p>
+        <p className="navSelectText">특가/혜택</p>
+      </div>
+      <p className="navDeliveryGuide">
+        <span>샛별 · 낮</span> 배송안내
+      </p>
+    </div>
+  );
 };
 
 export default NavBottomBox;
