502e5a4d0cff57f98e747c1726ac2faddc9eee95
yoonsue <mrokr0328@gmail.com>
2022-10-18 12:00:29 +0900

    feature/login: JSX 구현 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  return (
+    <div className="login">
+      <div className="title-container">
+        <div className="title">로그인</div>
+      </div>
+      <div className="form-container">
+        <form>
+          <div className="input-container">
+            <input
+              className="login-input "
+              placeholder="아이디를 입력해주세요"
+            ></input>
+            <input
+              className="login-input "
+              placeholder="비밀번호를 입력해주세요"
+            ></input>
+          </div>
+          <div className="find">
+            <Link to="/">아이디 찾기</Link>
+            <span></span>
+            <Link to="/">비밀번호 찾기</Link>
+          </div>
+          <div className="button-container">
+            <button className="login-button">로그인</button>
+            <button className="signin-button">회원가입</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
