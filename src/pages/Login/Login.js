04eb7d6d19de19421a728a6109c51a6dd32d2713
yoonsue <mrokr0328@gmail.com>
2022-10-19 14:51:54 +0900

    로그인 통신 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,7 +5,7 @@ import './Login.scss';
 function Login() {
   const navigate = useNavigate();
 
-  const [values, setValues] = useState({ userid: '', password: '' });
+  const [values, setValues] = useState({ userId: '', password: '' });
 
   //값을 받아서 인풋창에 세팅하는 함수입니다
   const onInputChange = e => {
@@ -14,50 +14,35 @@ function Login() {
   };
 
   //제출 시 새로고침을 막고 첫 번째로 값의 유무에 대한 validation을 진행하는 부분입니다
+  const isValid = values.userId && values.password;
+
   const onSubmit = e => {
-    if (
-      values.userid !== '' && values.password !== ''
-        ? alert('hello')
-        : alert(
-            '아이디 또는 비밀번호를 입력해주세요, 모달창이 띄워질 영역입니다'
-          )
-    ) {
-      alert('hello');
-      loginAuthorization();
-    }
     e.preventDefault();
-  };
-
-  const navigateToMain = () => {
-    navigate('/Main');
-  };
-
-  const loginAuthorization = () => {
-    fetch('', {
-      //로그인 API기입, 동일한 와이파이 접속 확인
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        userid: values.userid,
-        password: values.password,
-      }),
-    })
-      .then(response => {
-        if (response.ok === true) {
-          return response.json();
-        }
-        throw new Error('에러 발생, check status code');
+    if (!isValid) {
+      alert('아이디 또는 비밀번호를 입력해주세요, 모달창이 띄워질 영역입니다');
+    } else {
+      fetch('http://10.58.52.89:3000/users/signin', {
+        //로그인 API기입, 동일한 와이파이 접속 확인
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify(values),
       })
-      .catch(error => alert(error))
-      .then(data => {
-        if (data.message === 'login success') {
-          localStorage.setItem('TOKEN', data.accessToken); //'token'변수명에 대한 key값 통일 확인
-          alert('로그인 성공, 상태 확인용 alert입니다');
-          navigateToMain();
-        } else alert('로그인 실패, 모달창이 띄워질 예정입니다.');
-      });
+        .then(response => {
+          if (response.ok === true) {
+            return response.json();
+          }
+          throw new Error('에러 발생, check status code');
+        })
+        .catch(error => alert(error))
+        .then(data => {
+          if (data.message === 'LOGIN_SUCCESS') {
+            localStorage.setItem('token', data.token); //'token'변수명에 대한 key값 통일 확인
+            console.log(localStorage.getItem('token'));
+            navigate('/Main');
+          } else alert('로그인 실패, 모달창이 띄워질 예정입니다.');
+        });
+    }
   };
-  console.log(localStorage.getItem('TOKEN'));
 
   return (
     <div className="login">
@@ -68,10 +53,10 @@ function Login() {
         <form onSubmit={onSubmit}>
           <div className="input-container">
             <Input
-              value={values.userid}
+              value={values.userId}
               className="loginInput"
               type="id"
-              name="id"
+              name="userId"
               placeholder="아이디를 입력해주세요"
               onChange={onInputChange}
             />
@@ -90,7 +75,7 @@ function Login() {
             <Link to="/">비밀번호 찾기</Link>
           </div>
           <div className="buttonContainer">
-            <button className="loginButton" onSubmit={onSubmit}>
+            <button className="loginButton" onClick={onSubmit}>
               로그인
             </button>
             <Link to="/Signin">
