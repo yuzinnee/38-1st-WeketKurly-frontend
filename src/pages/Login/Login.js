f0ef81abaf12dbfe1c5e852fc259dde755fd4219
yoonsue <mrokr0328@gmail.com>
2022-10-19 11:23:53 +0900

    Add: Feature Login, prepared to communicate with Server

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({ userid: '', password: '' });
+
+  //값을 받아서 인풋창에 세팅하는 함수입니다
+  const onInputChange = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  //제출 시 새로고침을 막고 첫 번째로 값의 유무에 대한 validation을 진행하는 부분입니다
+  const onSubmit = e => {
+    if (
+      values.userid !== '' && values.password !== ''
+        ? alert('hello')
+        : alert(
+            '아이디 또는 비밀번호를 입력해주세요, 모달창이 띄워질 영역입니다'
+          )
+    ) {
+      alert('hello');
+      loginAuthorization();
+    }
+    e.preventDefault();
+  };
+
+  const navigateToMain = () => {
+    navigate('/Main');
+  };
+
+  const loginAuthorization = () => {
+    fetch('', {
+      //로그인 API기입, 동일한 와이파이 접속 확인
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({
+        userid: values.userid,
+        password: values.password,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('에러 발생, check status code');
+      })
+      .catch(error => alert(error))
+      .then(data => {
+        if (data.message === 'login success') {
+          localStorage.setItem('TOKEN', data.accessToken); //'token'변수명에 대한 key값 통일 확인
+          alert('로그인 성공, 상태 확인용 alert입니다');
+          navigateToMain();
+        } else alert('로그인 실패, 모달창이 띄워질 예정입니다.');
+      });
+  };
+  console.log(localStorage.getItem('TOKEN'));
+
   return (
     <div className="login">
       <div className="title-container">
         <div className="title">로그인</div>
       </div>
       <div className="form-container">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="input-container">
-            <input
-              className="login-input "
+            <Input
+              value={values.userid}
+              type="id"
+              className="login-input"
+              name="id"
               placeholder="아이디를 입력해주세요"
-            ></input>
-            <input
-              className="login-input "
+              onChange={onInputChange}
+            />
+            <Input
+              value={values.password}
+              type="password"
+              className="login-input"
+              name="password"
               placeholder="비밀번호를 입력해주세요"
-            ></input>
+              onChange={onInputChange}
+            />
           </div>
           <div className="find">
             <Link to="/">아이디 찾기</Link>
@@ -26,8 +90,12 @@ function Login() {
             <Link to="/">비밀번호 찾기</Link>
           </div>
           <div className="button-container">
-            <button className="login-button">로그인</button>
-            <button className="signin-button">회원가입</button>
+            <button className="login-button" onSubmit={onSubmit}>
+              로그인
+            </button>
+            <Link to="/Signin">
+              <button className="signin-button">회원가입</button>
+            </Link>
           </div>
         </form>
       </div>
@@ -36,3 +104,16 @@ function Login() {
 }
 
 export default Login;
+
+const Input = ({ className, onChange, name, value, type, placeholder }) => {
+  return (
+    <input
+      className={className}
+      onChange={onChange}
+      name={name}
+      value={value}
+      type={type}
+      placeholder={placeholder}
+    ></input>
+  );
+};
