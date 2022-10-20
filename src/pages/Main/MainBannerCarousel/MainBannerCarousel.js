345cca03b0a4b79c542dcd6f0f5f23ac72baad1d
zinukk <slsxpseh54@naver.com>
2022-10-20 14:42:45 +0900

    ADD : 메인컨텐츠박스와 메인배너케러셀 파일 추가

---
+++ src/pages/Main/MainBannerCarousel/MainBannerCarousel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './MainBannerCarousel.scss';
+const MainBannerCarousel = () => {
+  return <div></div>;
+};
+
+export default MainBannerCarousel;
---
+++ src/pages/Main/MainContentsBox/MainContentsBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './MainContentsBox.scss';
+const MainContentsBox = () => {
+  return <div></div>;
+};
+
+export default MainContentsBox;
