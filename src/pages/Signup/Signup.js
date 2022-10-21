d8b5ee94f6e36db33617e227d841e83ab565d604
yuzinnee <yuzinnee@naver.com>
2022-10-21 21:40:44 +0900

    Modify : 구현한 기능 오류 개선

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
-  const [asd, setAsd] = useState(false);
-  console.log(asd);
-
   const [values, setValues] = useState({
     userId: '',
     password: '',
@@ -16,11 +13,15 @@ const Signup = () => {
     month: '',
     day: '',
   });
-
-  const valueHandler = e => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+  const [isPasswordWarning, setIspasswordWarning] = useState(false);
+  const showPasswordWarning = () => {
+    setIspasswordWarning(true);
   };
-  console.log('values:', values);
+  const valueHandler = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   const {
     userId,
     password,
@@ -32,16 +33,21 @@ const Signup = () => {
     month,
     day,
   } = values;
+
   const userIdRegEx = /^[a-z]+[a-z0-9]{5,16}$/g;
-  const isUserIdVaild = userId.length > 5 && userId.length < 17;
-  const isPasswordValid = password.length > 9;
+  const userIdValid = userIdRegEx.test(userId);
+  const passwordRegEx =
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+  // / ^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const isPasswordValid = passwordRegEx.test(password);
   const isPasswordCheck = password === passwordCheck;
   const isNameValid = name.length > 0;
   const emailRegEx = /[a-zA-Z0-9+_]+@[a-z]+\.+[a-z]/;
   const isEmailValid = emailRegEx.test(email);
   const birthday = year + month.length === 2 ? month : 0 + month + day;
-  console.log(passwordCheck);
-  console.log(birthday);
+  console.log(password);
+  console.log(isPasswordWarning);
 
   const submitUseInfo = () => {
     fetch('http://10.58.52.89:3000/users/signup', {
@@ -76,6 +82,8 @@ const Signup = () => {
       });
   };
 
+  // function IdInput() {}
+
   return (
     <>
       <div className="signupContainer">
@@ -85,7 +93,6 @@ const Signup = () => {
             <span className="star">*</span>필수입력사항
           </div>
 
-          {/* ----------------------아이디아이디아이디아이디아이디---------------------------- */}
           <div className="eachContainer">
             <span className="leftLetter">
               아이디<span className="star">*</span>
@@ -99,7 +106,7 @@ const Signup = () => {
                 onChange={valueHandler}
               />
               {userId.length !== 0 &&
-                (isUserIdVaild || (
+                (userIdValid || (
                   <span className="warning">
                     6자 이상 16자 이하의 영문 혹은 숫자를 조합
                   </span>
@@ -109,7 +116,7 @@ const Signup = () => {
               <span className="buttonLetter">중복확인</span>
             </button>
           </div>
-          {/* ----------------------비번비번비번비번비번비번비번---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">
               비밀번호<span className="star">*</span>
@@ -121,14 +128,22 @@ const Signup = () => {
                 name="password"
                 placeholder="비밀번호를 입력해주세요"
                 onChange={valueHandler}
+                onFocus={showPasswordWarning}
               />
-              {password.length !== 0 &&
-                (isPasswordValid || (
+
+              {isPasswordWarning &&
+                (password.length < 10 ? (
                   <span className="warning">최소 10자 이상 입력</span>
+                ) : (
+                  isPasswordValid || (
+                    <span className="warning">
+                      영문 /숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합
+                    </span>
+                  )
                 ))}
             </div>
           </div>
-          {/* ----------------------비번확인비번확인비번확인---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">
               비밀번호확인<span className="star">*</span>
@@ -147,7 +162,7 @@ const Signup = () => {
                 ))}
             </div>
           </div>
-          {/* ----------------------이름이름이름이름이름---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">
               이름<span className="star">*</span>
@@ -165,7 +180,7 @@ const Signup = () => {
                 ))}
             </div>
           </div>
-          {/* ----------------------이멜이멜이멜이멜이멜이멜---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">
               이메일<span className="star">*</span>
@@ -189,7 +204,7 @@ const Signup = () => {
               <span className="buttonLetter">중복확인</span>
             </button>
           </div>
-          {/* ----------------------성별성별성별성별---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">성별</span>
             <div className="choiceContainer">
@@ -217,7 +232,7 @@ const Signup = () => {
               </div>
             </div>
           </div>
-          {/* ----------------------생년월일생년월일생년월일---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">생년월일</span>
             <div className="birthContainer">
@@ -247,7 +262,7 @@ const Signup = () => {
               ></input>
             </div>
           </div>
-          {/* ----------------------추가입력추가입력추가입력---------------------------- */}
+
           <div className="eachContainer">
             <span className="leftLetter">추가입력 사항</span>
             <div className="choiceContainer">
