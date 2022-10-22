eb4bf7f0c05eef0b5b3307787ece537daedcf606
zinukk <slsxpseh54@naver.com>
2022-10-22 16:29:34 +0900

    Modify : Carousel.js 컴포넌트 분리 및 computed property 적용

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import BannerCarousel from './BannerCarousel/BannerCarousel';
+import ItemCarousel from './ItemCarousel/ItemCarousel';
 import './Carousel.scss';
 
-const Carousel = () => {
-  return <div className="carousel"></div>;
+const Carousel = ({ type, contents }) => {
+  const carousel = {
+    banner: <BannerCarousel contents={contents} />,
+    item: <ItemCarousel contents={contents} />,
+  };
+  return <>{type[carousel]}</>;
 };
 
 export default Carousel;
