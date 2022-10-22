e8998f2a6d6f491cefc473b5676693211d84ac44
zinukk <slsxpseh54@naver.com>
2022-10-22 18:46:58 +0900

    ADD : bannerCarousel 구현 완료

--- src/components/Carousel/BannerCarousel/BannerCarousel.js
+++ src/components/Carousel/BannerCarousel/BannerCarousel.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import {
+  MdOutlineArrowForwardIos,
+  MdOutlineArrowBackIos,
+} from 'react-icons/md';
 import './BannerCarousel.scss';
 
 const BannerCarousel = () => {
-  return <div className="bannerCarousel"></div>;
+  const [slide, setSlide] = useState(0);
+
+  const [bannerList, setBannerList] = useState([]);
+
+  const slideRef = useRef(null);
+
+  const page = bannerList.length;
+
+  useEffect(() => {
+    fetch('data/DATA.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setBannerList(data.bannerCarousel);
+      });
+  }, []);
+
+  useEffect(() => {
+    slideRef.current.style.transition = 'all 0.4s ease-in-out';
+    slideRef.current.style.transform = `translateX(-${slide * 100}%)`;
+  }, [slide]);
+
+  const showNextSlide = () => {
+    setSlide(slide => slide + 1);
+  };
+  const showPrevSlide = () => {
+    setSlide(slide => slide - 1);
+  };
+
+  return (
+    <div className="bannerCarousel">
+      <div className="bannerCarouselBox" ref={slideRef}>
+        {bannerList.map((banner, idx) => (
+          <img
+            className="bannerImg"
+            src={banner.img}
+            alt="배너이미지"
+            key={banner.id}
+          />
+        ))}
+      </div>
+
+      {slide !== 0 && (
+        <div className="slideMinusBtn" onClick={showPrevSlide}>
+          <MdOutlineArrowBackIos className="slideMinusIcon" />
+        </div>
+      )}
+      {slide + 1 !== page && (
+        <div className="slidePlusBtn" onClick={showNextSlide}>
+          <MdOutlineArrowForwardIos className="slidePlusIcon" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BannerCarousel;
