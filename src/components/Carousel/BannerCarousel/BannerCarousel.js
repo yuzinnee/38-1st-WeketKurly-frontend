a3a74c4bbf45ba32274db33f50fb597befa1ceac
zinukk <slsxpseh54@naver.com>
2022-10-22 16:26:52 +0900

    ADD : BannerCarousel , ItemCarousel 폴더 및 파일 추가

---
+++ src/components/Carousel/BannerCarousel/BannerCarousel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BannerCarousel.scss';
+
+const BannerCarousel = () => {
+  return <div className="bannerCarousel"></div>;
+};
+
+export default BannerCarousel;
---
+++ src/components/Carousel/ItemCarousel/ItemCarousel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ItemCarousel.scss';
+
+const ItemCarousel = () => {
+  return <div className="itemCarousel"></div>;
+};
+
+export default ItemCarousel;
