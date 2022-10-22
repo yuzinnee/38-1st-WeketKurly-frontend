5e9b3dda29f6ae1b42754543c7b00f17b16d215a
zinukk <slsxpseh54@naver.com>
2022-10-22 21:12:26 +0900

    ADD : 타임특가 컴포넌트 구현 완료

--- src/pages/Main/MainContentsBox/MainSpecialPrice/MainSpecialPrice.js
+++ src/pages/Main/MainContentsBox/MainSpecialPrice/MainSpecialPrice.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MainSpecialPrice.scss';
+
 const MainSpecialPrice = () => {
-  return <div className="mainSpecialPrice">타임특가 자리</div>;
+  const calculateTimeLeft = () => {
+    const difference = +new Date('2022-10-24T24:00:00') - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        hours: Math.floor(difference / (1000 * 60 * 60)),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+  return (
+    <div className="mainSpecialPrice">
+      <div className="specialTimeBox">
+        <p className="specialDailyText">일일특가</p>
+        <p className="specialGrayText">24시간 한정 특가</p>
+        <span className="specialTimer">{timeLeft.hours}:</span>
+        <span className="specialTimer">
+          {timeLeft.minutes < 10
+            ? '0' + timeLeft.minutes + ':'
+            : timeLeft.minutes + ':'}
+        </span>
+        <span className="specialTimer">
+          {timeLeft.seconds < 10 ? '0' + timeLeft.seconds : timeLeft.seconds}
+        </span>
+        <p className="specialLgrayText">망설이면 늦어요!</p>
+      </div>
+      {/* Item 들어갈 위치 */}
+    </div>
+  );
 };
 
 export default MainSpecialPrice;
