23ea1378d4c64ff76bc229f0200b2d3f45cdd4ea
yoonsue <mrokr0328@gmail.com>
2022-10-23 15:41:17 +0900

    Add: Item 컴포넌트 추가 후 목데이터 패칭 완료

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './List.scss';
 import Sort from './Sort';
 
-const List = ({ subCategoryItems }) => {
+const List = () => {
   const [sortTypes, setSortTypes] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch('/data/SORTED_DATA.json')
@@ -13,6 +14,12 @@ const List = ({ subCategoryItems }) => {
       });
   }, []);
 
+  useEffect(() => {
+    fetch('/data/PRODUCTS.json', {})
+      .then(res => res.json())
+      .then(result => setProducts(result.getMainCategoriesAllProducts));
+  });
+
   return (
     <div className="listPageContainer">
       <div className="listWrapper">
@@ -20,8 +27,8 @@ const List = ({ subCategoryItems }) => {
         <div className="contentContainer">
           <Sort sortTypes={sortTypes} />
           <div className="listGrid">
-            {subCategoryItems.map(item => {
-              return <div key={item.id}>Item 컴포넌트가 들어올 자리입니다</div>;
+            {products.map((product, index) => {
+              return <DefaultItem key={index} contents={product} />;
             })}
           </div>
         </div>
@@ -31,3 +38,20 @@ const List = ({ subCategoryItems }) => {
 };
 
 export default List;
+
+// 요청 (request) - http://url:3000/categories/main/${maincategoriesId}
+
+const DefaultItem = ({ contents }) => {
+  return (
+    <div className="itemContainer">
+      <img
+        className="itemimg"
+        src="https://images.unsplash.com/photo-1666307533559-070b90d171a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+        alt="상품 이미지"
+      />
+
+      <p className="itemDescription">{contents?.productName}</p>
+      <p className="itemPrice">{contents?.productPrice + `원`}</p>
+    </div>
+  );
+};
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,30 @@
+// import React from 'react';
+// import MainBannerCarousel from './MainBannerCarousel/MainBannerCarousel';
+// import MainContentsBox from './MainContentsBox/MainContentsBox';
+// import Nav from '../../components/Nav/Nav';
+// import './Main.scss';
+
+// const Main = () => {
+//   return (
+//     <div className="main">
+//       <Nav />
+//       <MainBannerCarousel />
+//       <MainContentsBox />
+//     </div>
+//   );
+// };
+
+// export default Main;
 import React from 'react';
-import MainBannerCarousel from './MainBannerCarousel/MainBannerCarousel';
-import MainContentsBox from './MainContentsBox/MainContentsBox';
 import Nav from '../../components/Nav/Nav';
+import List from '../List/List';
 import './Main.scss';
 
 const Main = () => {
   return (
     <div className="main">
       <Nav />
-      <MainBannerCarousel />
-      <MainContentsBox />
+      <List />
     </div>
   );
 };
