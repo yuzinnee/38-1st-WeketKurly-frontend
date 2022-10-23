90c1cf0f74e840b16cd4e772443dd72804d244c5
zinukk <slsxpseh54@naver.com>
2022-10-23 21:26:00 +0900

    Modify : item key값 설정

--- src/components/Item/DiscountItem/DiscountItem.js
+++ src/components/Item/DiscountItem/DiscountItem.js
@@ -3,11 +3,15 @@ import { BsCart2 } from 'react-icons/bs';
 import './DiscountItem.scss';
 
 const DiscountItem = ({ contents }) => {
+  const discountPrice =
+    contents?.productPrice -
+    (contents?.discount / 100) * contents?.productPrice;
+
   return (
     <div className="disItemContainer">
       <img
         className="disItemImg"
-        src={contents?.item?.thumbnailImageUrl}
+        src={contents?.thumbnailImageUrl}
         alt="상품 이미지"
       />
       <div className="disItemIconBox">
@@ -17,9 +21,11 @@ const DiscountItem = ({ contents }) => {
       <div className="disItemSpecialBox">
         <p className="disItemSpecialText">일일특가</p>
       </div>
-
-      <p className="disItemDescription">{contents?.item?.productName}</p>
-      <p className="disItemPrice">{contents?.item?.price}</p>
+      <p className="disItemDescription">{contents?.shortDescription}</p>
+      <p className="disItemName">{contents?.productName}</p>
+      <span className="discountRate">{contents?.discount + `%`}</span>
+      <span className="disItemDiscountPrice">{discountPrice + `원`}</span>
+      <span className="disItemPrice">{contents?.productPrice}</span>
     </div>
   );
 };
