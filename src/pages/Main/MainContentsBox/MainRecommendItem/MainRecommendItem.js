e6a31c17397a41778dd32a8babff61b75e6d0d34
zinukk <slsxpseh54@naver.com>
2022-10-23 20:39:46 +0900

    Modify : fetch 함수 추가 및 수정

--- src/pages/Main/MainContentsBox/MainRecommendItem/MainRecommendItem.js
+++ src/pages/Main/MainContentsBox/MainRecommendItem/MainRecommendItem.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from '../../../../components/Carousel/Carousel';
 import './MainRecommendItem.scss';
 
 const MainRecommendItem = () => {
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://10.58.52.148:3000/categories', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    })
+      .then(res => res.json())
+      .then(data => {
+        setItemList(data.item);
+      });
+  }, []);
+
   return (
     <div className="mainRecommendItem">
-      <Carousel type="item" contents="이 상품은 어때요?" />
+      <Carousel type="item" title="이 상품은 어때요?" contents={itemList} />
     </div>
   );
 };
