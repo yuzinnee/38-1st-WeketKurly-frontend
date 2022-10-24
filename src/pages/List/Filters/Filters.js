97a617e118f8e1089eb97a60a1cdbb939789f283
yoonsue <mrokr0328@gmail.com>
2022-10-24 13:23:49 +0900

    Refactor: 불필요하게 import되는 컴포넌트 삭제

--- src/pages/List/Filters/Filters.js
+++ src/pages/List/Filters/Filters.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Filters.scss';
 
 const Filters = ({ data, fn, maincategoriesId }) => {
   return (
     <div className="filters-wrapper">
       <ul>
-        <li>전체보기</li>
+        <li>'maincategoriesId 에 맞는 한글 이름이 들어갈 예정입니다'</li>
         {data.map(item => {
           return (
             <NavLink
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,30 +1,15 @@
-// import React from 'react';
-// import MainBannerCarousel from './MainBannerCarousel/MainBannerCarousel';
-// import MainContentsBox from './MainContentsBox/MainContentsBox';
-// import Nav from '../../components/Nav/Nav';
-// import './Main.scss';
-
-// const Main = () => {
-//   return (
-//     <div className="main">
-//       <Nav />
-//       <MainBannerCarousel />
-//       <MainContentsBox />
-//     </div>
-//   );
-// };
-
-// export default Main;
 import React from 'react';
+import MainBannerCarousel from './MainBannerCarousel/MainBannerCarousel';
+import MainContentsBox from './MainContentsBox/MainContentsBox';
 import Nav from '../../components/Nav/Nav';
-import List from '../List/List';
 import './Main.scss';
 
 const Main = () => {
   return (
     <div className="main">
       <Nav />
-      <List />
+      <MainBannerCarousel />
+      <MainContentsBox />
     </div>
   );
 };
