e84a68b8fcaba035f0acdf763851ac939ec39fcd
zinukk <slsxpseh54@naver.com>
2022-10-25 15:13:34 +0900

    ADD : 서브배너 호출하는 fetch 함수 작성

--- src/pages/Main/MainContentsBox/MainSubBanner/MainSubBanner.js
+++ src/pages/Main/MainContentsBox/MainSubBanner/MainSubBanner.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import API from '../../../../config';
 import './MainSubBanner.scss';
 
 const MainSubBanner = () => {
-  const [banner, setBanner] = useState([]);
+  const [banner, setBanner] = useState('');
 
-  return (
-    <div className="mainSubBanner">
-      <img />
-    </div>
-  );
+  useEffect(() => {
+    fetch(API.mainSubBanner, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    })
+      .then(res => res.json())
+      .then(data => {
+        setBanner(data.message[0].image_url);
+      });
+  }, []);
+
+  return <img src={banner} className="mainSubBanner" alt="서브배너" />;
 };
 
 export default MainSubBanner;
