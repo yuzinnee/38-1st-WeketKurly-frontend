d2cdf4b6d938bf0c789a56945ae455f528021964
yuzinnee <yuzinnee@naver.com>
2022-10-26 16:34:20 +0900

    For Merge

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -4,34 +4,7 @@ import './Category.scss';
 
 const Category = () => {
   const [mainCategories, setMainCategories] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [isMouseEntered, setisMouseEntered] = useState(false);
-
-  // const openSubCategories = e =>
-  //   fetch('http://10.58.52.148:3000/categories')
-  //     .then(res => res.json())
-  //     .then(result => {
-  //       console.log(e.target.id);
-  //       setCategories(result.data[e.target.id - 1]?.subCategories);
-  //     });
-
-  // useEffect(() => {
-  //   fetch(' http://10.58.52.148:3000/categories')
-  //     .then(res => res.json())
-  //     .then(result => {
-  //       // console.log(result.data);
-  //       setMainCategories(result.data);
-  //     });
-  // }, []);
-
-  const openSubCategories = e => {
-    fetch('/data/CATEGORY.json')
-      .then(res => res.json())
-      .then(result => {
-        console.log(result);
-        setCategories(result[e.target.id - 1]?.subCategory);
-      });
-  };
+  const [isMouseEntered, setisMouseEntered] = useState('');
   useEffect(() => {
     fetch('/data/CATEGORY.json')
       .then(res => res.json())
@@ -45,21 +18,38 @@ const Category = () => {
     <div className="categoryContainer">
       <div className="mainCategory">
         {mainCategories?.map(maincategory => {
+          console.log(maincategory.mainCategoriesId);
           return (
             <li
               key={maincategory.mainCategoriesId}
               id={maincategory.mainCategoriesId}
               className="mainCategories"
-              onMouseEnter={}
+              onMouseEnter={() => setisMouseEntered(true)}
+              onMouseLeave={() => setisMouseEntered(false)}
             >
               {maincategory.mainCategoriesName}
             </li>
           );
         })}
       </div>
-      {isMouseEntered && (
+      {isMouseEntered ? (
+        <div className="subCategory">
+          {mainCategories?.map(category => {
+            console.log(category);
+            return (
+              <li key={subCategory.subCategoriesName} className="subCategories">
+                .subCategory.map(subCategory => {
+                  return subCategory.subCategoriesName;
+                })}
+              </li>
+            );
+          })}
+        </div>
+      ) : null}
+      {/* {isMouseEntered ? (
         <div className="subCategory">
           {categories?.map(category => {
+            console.log(categories);
             return (
               <li key={category.subCategoriesId} className="subCategories">
                 {category.subCategoriesName}
@@ -67,9 +57,44 @@ const Category = () => {
             );
           })}
         </div>
-      )}
+      ) : null} */}
+      {/* <div className="subCategory">
+        {categories?.map(category => {
+          return (
+            <li key={category.subCategoriesId} className="subCategories">
+              {category.subCategoriesName}
+            </li>
+          );
+        })}
+      </div> */}
     </div>
   );
 };
 
 export default Category;
+
+// const openSubCategories = e =>
+//   fetch('http://10.58.52.148:3000/categories')
+//     .then(res => res.json())
+//     .then(result => {
+//       console.log(e.target.id);
+//       setCategories(result.data[e.target.id - 1]?.subCategories);
+//     });
+
+// useEffect(() => {
+//   fetch(' http://10.58.52.148:3000/categories')
+//     .then(res => res.json())
+//     .then(result => {
+//       // console.log(result.data);
+//       setMainCategories(result.data);
+//     });
+// }, []);
+
+// const openSubCategories = e => {
+//   fetch('/data/CATEGORY.json')
+//     .then(res => res.json())
+//     .then(result => {
+//       console.log(result);
+//       setCategories(result[e.target.id - 1]?.subCategory);
+//     });
+// };
