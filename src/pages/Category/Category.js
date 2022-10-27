55179de4b63f4d9a6dc805e6a3c6da342068426b
yuzinnee <yuzinnee@naver.com>
2022-10-27 11:03:38 +0900

    Add : category 기능구현 완료

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -3,98 +3,59 @@ import { useEffect, useState } from 'react';
 import './Category.scss';
 
 const Category = () => {
-  const [mainCategories, setMainCategories] = useState([]);
-  const [isMouseEntered, setisMouseEntered] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [categoryId, setCategoryId] = useState(null);
+
   useEffect(() => {
     fetch('/data/CATEGORY.json')
       .then(res => res.json())
       .then(result => {
-        console.log(result);
-        setMainCategories(result);
+        setCategories(result);
       });
   }, []);
 
+  const findSubCategory = id => {
+    const subCategory = categories.find(
+      category => +category.mainCategoriesId === +id
+    );
+
+    if (!subCategory) return [];
+    else return subCategory.subCategory;
+  };
+
   return (
     <div className="categoryContainer">
-      <div className="mainCategory">
-        {mainCategories?.map(maincategory => {
-          console.log(maincategory.mainCategoriesId);
+      <div className="mainCategoryFrame">
+        {categories.map(category => {
           return (
             <li
-              key={maincategory.mainCategoriesId}
-              id={maincategory.mainCategoriesId}
-              className="mainCategories"
-              onMouseEnter={() => setisMouseEntered(true)}
-              onMouseLeave={() => setisMouseEntered(false)}
+              key={category.id}
+              id={category.mainCategoriesId}
+              className="mainCategoriesName"
+              onMouseEnter={() => {
+                setCategoryId(category.mainCategoriesId);
+              }}
             >
-              {maincategory.mainCategoriesName}
+              {category.mainCategoriesName}
             </li>
           );
         })}
       </div>
-      {isMouseEntered ? (
-        <div className="subCategory">
-          {mainCategories?.map(category => {
-            console.log(category);
-            return (
-              <li key={subCategory.subCategoriesName} className="subCategories">
-                .subCategory.map(subCategory => {
-                  return subCategory.subCategoriesName;
-                })}
-              </li>
-            );
-          })}
-        </div>
-      ) : null}
-      {/* {isMouseEntered ? (
-        <div className="subCategory">
-          {categories?.map(category => {
-            console.log(categories);
-            return (
-              <li key={category.subCategoriesId} className="subCategories">
-                {category.subCategoriesName}
-              </li>
-            );
-          })}
-        </div>
-      ) : null} */}
-      {/* <div className="subCategory">
-        {categories?.map(category => {
+      <div className="subcategoryFrame" onMouseLeave={() => setCategoryId(0)}>
+        {findSubCategory(categoryId).map(category => {
           return (
-            <li key={category.subCategoriesId} className="subCategories">
-              {category.subCategoriesName}
+            <li
+              key={category.subCategoriesId}
+              id={category.subCategoriesId}
+              className="subCategoriesName"
+            >
+              {category?.subCategoriesName}
             </li>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 };
 
 export default Category;
-
-// const openSubCategories = e =>
-//   fetch('http://10.58.52.148:3000/categories')
-//     .then(res => res.json())
-//     .then(result => {
-//       console.log(e.target.id);
-//       setCategories(result.data[e.target.id - 1]?.subCategories);
-//     });
-
-// useEffect(() => {
-//   fetch(' http://10.58.52.148:3000/categories')
-//     .then(res => res.json())
-//     .then(result => {
-//       // console.log(result.data);
-//       setMainCategories(result.data);
-//     });
-// }, []);
-
-// const openSubCategories = e => {
-//   fetch('/data/CATEGORY.json')
-//     .then(res => res.json())
-//     .then(result => {
-//       console.log(result);
-//       setCategories(result[e.target.id - 1]?.subCategory);
-//     });
-// };
