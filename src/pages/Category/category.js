d11cf269d831fb0e111ff7afcf2e9de19e5a50c8
yuzinnee <yuzinnee@naver.com>
2022-10-27 22:14:21 +0900

    Test: 서버 통신 테스트

--- src/pages/Category/category.js
+++ src/pages/Category/category.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import API from '../../config';
 import './Category.scss';
 
-const Category = () => {
+const Category = ({ setOpenCategory }) => {
   const [categories, setCategories] = useState([]);
   const [categoryId, setCategoryId] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('http://10.58.52.148:3000/categories')
+    fetch(API.category, {
+      method: 'GET',
+    })
       .then(res => res.json())
       .then(result => {
         setCategories(result.data);
@@ -21,37 +24,45 @@ const Category = () => {
     const subCategory = categories.find(
       category => +category.mainCategoriesId === +id
     );
+
     if (!subCategory) return [];
     else return subCategory.subCategories;
   };
 
   return (
-    <div className="categoryContainer">
+    <div
+      className="categoryContainer"
+      onMouseLeave={() => setOpenCategory(false)}
+    >
       <div className="mainCategoryFrame">
-        {categories &&
-          categories.map(category => {
-            return (
-              <
-                key={category.mainCategoriesId}
-                id={category.mainCategoriesId}
-                className="mainCategoriesName"
-                onMouseEnter={() => {
-                  setCategoryId(category.mainCategoriesId);
-                }}
-                onClick={() => navigate(`/list/${category.mainCategoriesId}`)}
-              >
-                {category.mainCategoriesName}
-              </li>
-            );
-          })}
+        {categories.map(category => {
+          return (
+            <li
+              key={category.mainCategoriesId}
+              id={category.mainCategoriesId}
+              className="mainCategoriesName"
+              onMouseEnter={() => {
+                setCategoryId(category.mainCategoriesId);
+              }}
+              onClick={() => {
+                navigate(`/list/${category.mainCategoriesId}`);
+              }}
+            >
+              {category.mainCategoriesName}
+            </li>
+          );
+        })}
       </div>
       <div className="subcategoryFrame" onMouseLeave={() => setCategoryId(0)}>
-        {findSubCategory(categoryId).map(category => {
+        {findSubCategory(categoryId)?.map(category => {
           return (
             <li
               key={category.subCategoriesId}
               id={category.subCategoriesId}
               className="subCategoriesName"
+              onClick={() => {
+                navigate(`/list/sub/${category.subCategoriesId}`);
+              }}
             >
               {category?.subCategoriesName}
             </li>
